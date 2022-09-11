20a98f4ebc87cb783c8a6d72ee4f03f1b0d4cbb0
nabaraj <nabaraj2k@gmail.com>
2022-09-11 21:25:11 +0530

    test cases fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { sortingChallenge } from "./utils";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("test sortingChallenge", () => {
+  const dummyData = [
+    {
+      rating: 14,
+      registered: 1089589325175.3094,
+    },
+    { rating: 32, registered: 1651379053910.9229 },
+    { rating: 3, registered: 666157597867.0438 },
+  ];
+  let result = sortingChallenge(dummyData, "date");
+  expect(result).toEqual([
+    { rating: 32, registered: 1651379053910.9229 },
+    { rating: 14, registered: 1089589325175.3094 },
+    { rating: 3, registered: 666157597867.0438 },
+  ]);
+});
+test("test sortingChallenge", () => {
+  const dummyData = [
+    {
+      rating: 14,
+      registered: 1089589325175.3094,
+    },
+    { rating: 2, registered: 1651379053910.9229 },
+    { rating: 3, registered: 666157597867.0438 },
+  ];
+  let result = sortingChallenge(dummyData, "rating");
+  expect(result).toEqual([
+    {
+      rating: 14,
+      registered: 1089589325175.3094,
+    },
+    { rating: 3, registered: 666157597867.0438 },
+    { rating: 2, registered: 1651379053910.9229 },
+  ]);
 });
